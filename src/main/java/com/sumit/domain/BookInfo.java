401acb98539a3c82c37473780e9db8df878dc0a1
The-Quantum-Boy <dhanorkar.sumit.code@gmail.com>
2023-05-24 09:06:58 +0530

    domain added

---
+++ src/main/java/com/sumit/domain/BookInfo.java
@@ -0,0 +1,19 @@
+package com.sumit.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookInfo {
+
+    private long bookId;
+
+    private String title;
+
+    private String author;
+
+    private String ISBN;
+}
